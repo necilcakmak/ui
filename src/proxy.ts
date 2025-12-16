a4b1b26c8cf8fb3ad7140b6932a82446f65af39d
Necil <61680106+necilcakmak@users.noreply.github.com>
2025-12-16 14:02:35 +0300

    Update proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -11,13 +11,15 @@ export function proxy(request: NextRequest) {
   try {
     const payload = token.split(".")[1];
     const decoded = JSON.parse(Buffer.from(payload, "base64").toString());
-
+    console.log("DEBUG: Decoded Payload:", decoded); // Rol ismini buradan göreceğiz
     const userRole =
       decoded["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"];
+    console.log("DEBUG: Kullanıcı Rolü:", userRole);
     if (userRole !== "Admin") {
       return NextResponse.redirect(new URL("/404", request.url));
     }
   } catch (err) {
+    console.error("DEBUG: Decode Hatası:", err.message);
     return NextResponse.redirect(new URL("/site/login", request.url));
   }
 
